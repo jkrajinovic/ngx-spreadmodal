80194e9ba1a549893d63fbe4538b0a17563e7d82
jkrajinovic <jakrajinovic@gmail.com>
2018-11-12 16:19:25 +0100

    fix: promise returns correct type

--- projects/ngx-spreadmodal/src/lib/ngx-spreadmodal.component.ts
+++ projects/ngx-spreadmodal/src/lib/ngx-spreadmodal.component.ts
@@ -22,7 +22,7 @@ export class NgxSpreadmodalComponent {
 
   constructor() { }
 
-  openModal(): Promise<any> {
+  openModal(): Promise<NgxSpreadmodalComponent> {
 
     this.open = true;
 
@@ -39,7 +39,7 @@ export class NgxSpreadmodalComponent {
 
   }
 
-  closeModal() {
+  closeModal(): Promise<NgxSpreadmodalComponent> {
     
     this.showContent = false;
 
