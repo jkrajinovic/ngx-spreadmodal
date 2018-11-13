c4cf7b0fe72b5ca168ec557262e965b696bacaec
jkrajinovic <jakrajinovic@gmail.com>
2018-11-13 13:40:48 +0100

    Add: unit tests added

--- projects/ngx-spreadmodal/src/lib/ngx-spreadmodal.component.spec.ts
+++ projects/ngx-spreadmodal/src/lib/ngx-spreadmodal.component.spec.ts
@@ -5,6 +5,16 @@ import { NgxSpreadmodalComponent } from './ngx-spreadmodal.component';
 describe('NgxSpreadmodalComponent', () => {
   let component: NgxSpreadmodalComponent;
   let fixture: ComponentFixture<NgxSpreadmodalComponent>;
+  let initialProperties = {};
+
+  beforeEach(async(() => {
+    initialProperties = {
+      open: false,
+      showContent: false,
+      spreadAnimateDuration: 700,
+      contentAnimateDuration: 700
+    }
+  }));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,4 +32,26 @@ describe('NgxSpreadmodalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('expect initialProperties with values to be defined', () => {
+    expect(component).toEqual(jasmine.objectContaining(initialProperties));
+  });
+  
+  it('#openModal() should set #open and #showContent to true', (done) => {
+    component.openModal().then(() => {
+      expect(component.open).toBe(true);
+      expect(component.showContent).toBe(true, 'on after click');
+      done();
+    });
+  });
+
+
+  it('#closeModal() should set #open and #showContent to false', (done) => {
+    component.closeModal().then(() => {
+      expect(component.open).toBe(false);
+      expect(component.showContent).toBe(false, 'on after click');
+      done();
+    });
+  });
+
 });
