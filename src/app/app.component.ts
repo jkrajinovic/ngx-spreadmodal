1b4281042e1f3d4dbab3984793124ed0afd4226d
jkrajinovic <jakrajinovic@gmail.com>
2018-11-14 14:14:00 +0100

    add: demo page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { NgxSpreadmodalComponent } from './../../projects/ngx-spreadmodal/src/lib/ngx-spreadmodal.component';
+// import { NgxSpreadmodalComponent } from 'ngx-spreadmodal';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-spreadmodal';
+  @ViewChild('spread') spread:NgxSpreadmodalComponent;
+
+
+  openDialog(){
+    console.log('opening modal');
+    this.spread.openModal().then(() =>{
+      console.log('opened');
+      
+    });
+  }
+
+  submitForm(){
+    console.log('closing modal with submit');
+    this.spread.closeModal().then(() =>{
+      console.log('closed');
+    });
+  }
+
+  cancel(){
+    console.log('closing modal with cancel');
+    this.spread.closeModal().then(() =>{
+      console.log('closed');
+    });
+  }
+  
 }
